be9c8e1946a16e62cd4067bfcd9f74f3fc976c76
mengiefen <mengiefen@gmail.com>
2022-03-18 11:51:00 +0300

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class App extends Component {
+export default class App extends React.PureComponentComponent {
   render() {
-    return (
-      <div>App</div>
-    )
+    return <div>App</div>;
   }
 }

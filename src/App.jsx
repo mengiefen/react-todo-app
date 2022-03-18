f104f6ff4f61fca5f04c151fa8e380470b90616c
mengiefen <mengiefen@gmail.com>
2022-03-18 15:32:29 +0300

    aadd App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import TodoContainer from './components/TodoContainer';
+import Header from './components/Header/Header';
 
-export default class App extends React.PureComponentComponent {
+export default class App extends React.PureComponent {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <Header />
+        <TodoContainer />
+      </div>
+    );
   }
 }

0c2fa3a70ea99cff354ce027116599d32f106af5
mengiefen <mengiefen@gmail.com>
2022-03-18 18:19:14 +0300

    refactor to function based component

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,14 +1,14 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import TodoInput from './TodoInput/TodoInput';
 import TodoList from './TodoList/TodoList';
 
-export default class TodoContainer extends PureComponent {
-  render() {
-    return (
-      <div>
-        <TodoInput />
-        <TodoList />
-      </div>
-    );
-  }
+function TodoContainer() {
+  return (
+    <div>
+      <TodoInput />
+      <TodoList />
+    </div>
+  );
 }
+
+export default TodoContainer;

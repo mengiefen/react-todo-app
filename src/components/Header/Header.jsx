9ff4e954a386a10cc7aef5024014bacae80f078f
mengiefen <mengiefen@gmail.com>
2022-03-18 15:31:32 +0300

    add header element

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class Header extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Todos with Drag and Drop Option</h1>
+      </div>
+    );
+  }
+}

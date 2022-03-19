72bde1c265875c1625258bb07492c7b8501b6df8
mengiefen <mengiefen@gmail.com>
2022-03-19 21:50:56 +0300

    add overdue component

---
+++ src/components/TodoOverDue/TodoOverDue.jsx
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import styles from './TodoOverDue.module.css';
+
+export default class TodoOverDue extends PureComponent {
+  render() {
+    const todoItemCompleted = {
+      borderLeft: '1rem solid #004400',
+    };
+    const statusCompleted = {
+      color: 'green',
+    };
+    const {
+      id,
+      title,
+      completed,
+      handleChecked,
+      onDelete,
+    } = this.props;
+    return (
+      <div
+        className={styles.todoItem}
+        style={completed ? todoItemCompleted : {}}
+      >
+        <input
+          type="checkbox"
+          className={styles.todoCheck}
+          checked={completed}
+          onChange={() => handleChecked(id)}
+        />
+        <p className={styles.todoText}>{title}</p>
+        <span
+          className={styles.todoStatus}
+          style={completed ? statusCompleted : {}}
+        >
+          STATUS
+        </span>
+        <button
+          type="button"
+          className={styles.todoDelete}
+          onClick={() => onDelete(id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+}
+
+TodoOverDue.propTypes = {
+  title: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+  handleChecked: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+};

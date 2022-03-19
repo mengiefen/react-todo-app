f8d28d9090e69ad19d6e868ba5efebc17a48629a
mengiefen <mengiefen@gmail.com>
2022-03-20 00:02:45 +0300

    refactor TodoContainer.jsx

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,107 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 as uid } from 'uuid';
 import TodoInput from './TodoInput/TodoInput';
 import TodoList from './TodoList/TodoList';
 import TodoListCompleted from './TodoListCompleted/TodoListCompleted';
 
-class TodoContainer extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-    };
-  }
-
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/todos')
-  //     .then((response) => response.json())
-  //     .then((data) => this.setState({ todos: data }));
-  // }
-
-  componentDidMount() {
+function TodoContainer() {
+  const getInitialTodos = () => {
     const temp = localStorage.getItem('todos');
-    const loadedTodos = JSON.parse(temp);
-    if (loadedTodos) {
-      this.setState({
-        todos: loadedTodos,
-      });
-    }
-  }
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  };
+  const [todos, setTodos] = useState(getInitialTodos());
 
-  componentDidUpdate(prevProps, prevState) {
-    const { todos } = this.state;
-    if (prevState.todos !== todos) {
-      const temp = JSON.stringify(todos);
-      localStorage.setItem('todos', temp);
-    }
-  }
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
 
-  handleTodoEdit = (text, id) => {
-    const { todos } = this.state;
-    this.setState({
-      todos: todos.map((todo) => {
+  const handleTodoEdit = (text, id) => {
+    setTodos(
+      todos.map((todo) => {
         if (todo.id === id) {
           return { ...todo, title: text };
         }
         return todo;
       }),
-    });
+    );
   };
 
-  handleDelete = (id) => {
-    this.setState((prevState) => ({
-      todos: prevState.todos.filter((todo) => todo.id !== id),
+  const handleDelete = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
+
+  const handleChecked = (id) => {
+    setTodos((prevState) => prevState.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
     }));
   };
 
-  handleChecked = (id) => {
-    this.setState((prevState) => ({
-      todos: prevState.todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      }),
-    }));
+  const handleSubmit = (title) => {
+    setTodos([...todos, { id: uid(), title, completed: false }]);
   };
 
-  handleSubmit = (title) => {
-    this.setState((prevState) => ({
-      todos: [
-        ...prevState.todos,
-        { id: uid(), title, completed: false },
-      ],
-    }));
-  };
+  const onDueTodos = todos.filter((todo) => todo.completed === false);
+  const compltedTodos = todos.filter(
+    (todo) => todo.completed === true,
+  );
 
-  render() {
-    const { todos } = this.state;
-    const onDueTodos = todos.filter(
-      (todo) => todo.completed === false,
-    );
-    const compltedTodos = todos.filter(
-      (todo) => todo.completed === true,
-    );
-    return (
-      <>
-        <TodoInput handleSubmit={this.handleSubmit} />
-        <div className="todo-holder">
-          <TodoList
-            todos={onDueTodos}
-            handleChecked={this.handleChecked}
-            handleDelete={this.handleDelete}
-            handleTodoEdit={this.handleTodoEdit}
-          />
-          <TodoListCompleted
-            todos={compltedTodos}
-            handleChecked={this.handleChecked}
-            handleDelete={this.handleDelete}
-            handleTodoEdit={this.handleTodoEdit}
-          />
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <TodoInput handleSubmit={handleSubmit} />
+      <div className="todo-holder">
+        <TodoList
+          todos={onDueTodos}
+          handleChecked={handleChecked}
+          handleDelete={handleDelete}
+          handleTodoEdit={handleTodoEdit}
+        />
+        <TodoListCompleted
+          todos={compltedTodos}
+          handleChecked={handleChecked}
+          handleDelete={handleDelete}
+          handleTodoEdit={handleTodoEdit}
+        />
+      </div>
+    </>
+  );
 }
 
 export default TodoContainer;
